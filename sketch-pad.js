8014ac8b6644b9574f803543a2e291f9e16daa6b
Simon Lin <patternblue1441@gmail.com>
2015-06-19 03:18:36 -0400

    update sketch darkening

--- sketch-pad.js
+++ sketch-pad.js
@@ -4,46 +4,50 @@ function theMain(){
 	
 	//create container
 	$('body').append('<div class="container"></div>');
-	$('.container').append('<button>Reset</button>', '<div class="theGrid"></div>');
+	$('.container').append('<button>New Grid!</button>', '<div class="theGrid"></div>');
 	var $theGrid = $('.theGrid');
 	var totalWidth = 960;
-	var totalHeight = 960;
+	var totalHeight = 700;
 	$theGrid.width(totalWidth);
 	$theGrid.height(totalHeight);
-
 
 	createSquares;
 
 	//change grid size when button pressed
-
 	$("button").on("click",createSquares);
 
+	var lessColor = 75;
+
+	//make darker when mouse hovers over square
 	$theGrid.on('mouseenter', '.square', function(){
+		var rgb = $(this).css('background-color').match(/\d+/g);		
+
+		// get new color values
+		var rNew = changeColor(rgb[0]);
+		var gNew = changeColor(rgb[1]);
+		var bNew = changeColor(rgb[2]);
+
+		theColor = '#' + rNew + gNew + bNew;
 		$(this).css({
-	 		'background-color':'#000000'
+	 		'background-color':theColor
 		});
 	});
-	//change color when hovered
-	// $(".square").ready(function(){
-	// 	console.log('testing');
-	// });
 
-	// $(".square").hover(function(){
-	//  	// console.log('testing');
-	//  	$(this).addClass('fillThis');
-	 
-	//  });
+	// set rgb component value (0-255) to 
+	function changeColor(rgbComponent){
+		// change string value to int, then operate
+		var newInteger = parseInt(rgbComponent,10) - lessColor;
+		//set number to 0 if negative, change to hex string with two digits (e.g. 0C)
+		hexComponent = ('0' + Math.max(0, newInteger).toString(16)).slice(-2);
+		return hexComponent;
+	}
 
-	// $(".fillThis").css({
-	//  		'background-color':'#000000'
-	// });
-
+	//create grid with squares
 	function createSquares(){
 
 		var sizeOfGrid = prompt("Enter a number for NxN grid");		
 		var widthOfSquare = totalWidth/sizeOfGrid;
 		var heightOfSquare = totalHeight/sizeOfGrid;
-
 		$theGrid.empty();
 		//generate 16x16 divs
 		for(i = 0; i < sizeOfGrid*sizeOfGrid; i++){
@@ -58,16 +62,9 @@ function theMain(){
 			'float':'left',
 			'width': widthOfSquare,
 			'height': heightOfSquare,
-			'background-color':'#999999',
-			'outline':'1px solid white'
+			'background-color':'#ffffff',
+			'outline':'1px solid black'
 		});
-	};
-
-	// function changeColor(){
-	// 	$(this).css({
-	// 		'background-color':'#000000'
-	// 	});
-	// };
-
+	}
 
 }
